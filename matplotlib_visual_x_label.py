5aadb74688fb632fc992275408b9ababc1e4fa74
fadz <ahfadz.jelliut@gmail.com>
2025-11-25 22:06:19 +0800

    Create matplotlib_visual_x_label.py

---
+++ matplotlib_visual_x_label.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+table = pd.read_csv("")
+table.head()
+
+x_label = table['a']
+plt.bar(x=np.arange(len(x_label)),height=table['a'])
+plt.xticks(np.arange(len(x_label)),table['a'], rotation=30)
+plt.show()
