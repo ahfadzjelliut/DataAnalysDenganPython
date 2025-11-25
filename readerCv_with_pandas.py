96be480ddfc198a0a05235d9eef4e9da4fcad3a4
fadz <ahfadz.jelliut@gmail.com>
2025-11-25 21:47:34 +0800

    Update readerCv_with_pandas.py

--- readerCv_with_pandas.py
+++ readerCv_with_pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 pd.set_option("display.max_columns",50)
 
-table = pd.read_csv("")
+table = pd.read_csv("https://opendata.limapuluhkotakab.go.id/download/c01e5dc1d2b9467a944519ec10c45862.csv")
 table.head()
 print(table)
